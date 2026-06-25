1756d2ba5b1098c7fecb72642b57b941d1a15780
Christopher Perez <Data.Christopher.Perez@Gmail.com>
2026-06-25 02:13:28 -0400

    update

--- server/app.py
+++ server/app.py
@@ -18,6 +18,12 @@ db.init_app(app)
 
 api = Api(app)
 
+@app.before_request
+def check_if_logged_in():
+    allowed = ['member_index', 'member_article']
+    if request.endpoint in allowed and not session.get('user_id'):
+        return {'error': 'Unauthorized'}, 401
+
 class ClearSession(Resource):
 
     def delete(self):
@@ -85,15 +91,21 @@ class CheckSession(Resource):
         return {}, 401
 
 class MemberOnlyIndex(Resource):
-    
+        
     def get(self):
-        pass
+    
+        articles = [ArticleSchema().dump(article) for article in Article.query.filter(Article.is_member_only == True).all()]
+    
+        return articles, 200
 
 class MemberOnlyArticle(Resource):
     
     def get(self, id):
-        pass
 
+        article = Article.query.filter(Article.id == id).first()
+        
+        return ArticleSchema().dump(article), 200
+        
 api.add_resource(ClearSession, '/clear', endpoint='clear')
 api.add_resource(IndexArticle, '/articles', endpoint='article_list')
 api.add_resource(ShowArticle, '/articles/<int:id>', endpoint='show_article')
